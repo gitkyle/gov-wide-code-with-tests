c97ea79ff47418344933d0b35dff45fc5de7e510
Kyle Hicks <khicks1124@gmail.com>
2020-05-07 12:33:21 -0400

    testing CI, push after PR created

--- Universal-Federated-Analytics.lib.js
+++ Universal-Federated-Analytics.lib.js
@@ -10,7 +10,7 @@ var oCONFIG = {
 
   AGENCY: '',
   SUB_AGENCY: '',
-  VERSION: '20200416 v5.0 - Universal Analytics',
+  VERSION: '20200416 v5.01 - Universal Analytics',
   SITE_TOPIC: '',
   SITE_PLATFORM: '',
   SCRIPT_SOURCE: '',
